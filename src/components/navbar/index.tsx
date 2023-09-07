3a5e00b7058292a3754f155d6a4615c30d739e34
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-09-07 18:57:26 +0400

    Improved how navbar looks

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <div className="flex px-3 py-2">
+    <div className="flex px-10 py-5">
       <Link
         to={"/"}
-        className="text-2xl font-bold hover:text-primary transition"
+        className="text-3xl font-bold hover:text-primary transition"
       >
         NeoJellyfin
       </Link>
