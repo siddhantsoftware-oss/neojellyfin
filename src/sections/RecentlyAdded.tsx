acda79651214e9f7092b8eef4063239bb08772d2
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-09-07 17:33:59 +0400

    Only shows Logo for shows where it exists

--- src/sections/RecentlyAdded.tsx
+++ src/sections/RecentlyAdded.tsx
@@ -30,14 +30,32 @@ function RecentlyAdded() {
 
   return (
     <div className="flex py-10 flex-col gap-y-3">
-      <Marquee speed={30} pauseOnHover>
+      <Marquee speed={40} pauseOnHover>
         {data?.map((media, idx) => (
           <div
             key={idx}
             className="shrink-0 group/cover relative transition overflow-clip mx-2 rounded-md"
           >
-            <div></div>
-            <div className="absolute top-0 left-0 h-full w-full transition group-hover/cover:bg-black/40 group-hover/cover:backdrop-blur-sm "></div>
+            <div className="absolute top-0 left-0 h-full w-full transition group-hover/cover:bg-black/40 group-hover/cover:backdrop-blur-sm ">
+              <div className="group-hover/cover:visible flex flex-col invisible transition p-5">
+                <div>
+                  <img
+                    id={media.SeriesId ?? media.Id}
+                    src={`${localStorage.getItem("address")}/Items/${
+                      media.SeriesId ?? media.Id
+                    }/Images/Logo`}
+                    className="w-[200px] h-fit "
+                    alt={"Movie poster for " + media.Name}
+                    onError={() => {
+                      const element = document.getElementById(
+                        media.SeriesId ?? media.Id
+                      );
+                      element?.parentNode?.removeChild(element);
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
             <img
               width={600}
               className="h-fit "
