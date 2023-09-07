749fbbd60f35413f77b43d8e919507f702d8174b
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-09-07 18:18:02 +0400

    Added hashblur

--- src/sections/RecentlyAdded.tsx
+++ src/sections/RecentlyAdded.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { getAuth } from "../pages/root";
 import Marquee from "react-fast-marquee";
 import { Link } from "react-router-dom";
+import { Blurhash } from "react-blurhash";
 
 interface MediaType {
   Name: string;
@@ -15,6 +16,13 @@ interface MediaType {
   PremiereDate: Date;
   hasSubtitles: boolean;
   Type: string;
+  BackdropImageTags: string[];
+  ParentBackdropImageTags: string[];
+  ImageBlurHashes: {
+    Backdrop: {
+      [key: string]: string;
+    };
+  };
 }
 
 function RecentlyAdded() {
@@ -36,63 +44,82 @@ function RecentlyAdded() {
   return (
     <div className="flex py-10 flex-col gap-y-3">
       <Marquee speed={40} pauseOnHover>
-        {data?.map((media, idx) => (
-          <div
-            key={idx}
-            className="shrink-0 group/cover relative transition overflow-clip mx-2 rounded-md"
-          >
-            <div className="absolute top-0 left-0 h-full w-full transition group-hover/cover:bg-black/40 group-hover/cover:backdrop-blur-sm ">
-              <div className="group-hover/cover:visible h-full flex flex-col justify-between invisible transition p-5">
-                <div className="flex flex-col gap-y-5">
-                  <div>
-                    <img
-                      id={media.SeriesId ?? media.Id}
-                      src={`${localStorage.getItem("address")}/Items/${
-                        media.SeriesId ?? media.Id
-                      }/Images/Logo`}
-                      className="w-[200px] h-fit "
-                      alt={"Movie poster for " + media.Name}
-                      onError={() => {
-                        const element = document.getElementById(
+        {data?.map((media, idx) => {
+          const backdropTag =
+            media.BackdropImageTags[0] ?? media.ParentBackdropImageTags[0];
+          const hashblur =
+            media.ImageBlurHashes.Backdrop[`${backdropTag}`] ?? "";
+
+          return (
+            <div
+              key={idx}
+              className="shrink-0 group/cover relative transition overflow-clip mx-2 rounded-md"
+            >
+              <div className="absolute top-0 left-0 h-full w-full transition group-hover/cover:bg-black/40 group-hover/cover:backdrop-blur-sm ">
+                <div className="group-hover/cover:visible h-full flex flex-col justify-between invisible transition p-5">
+                  <div className="flex flex-col gap-y-5">
+                    <div className="">
+                      <img
+                        id={media.SeriesId ?? media.Id}
+                        src={`${localStorage.getItem("address")}/Items/${
                           media.SeriesId ?? media.Id
-                        );
-                        element?.parentNode?.removeChild(element);
-                      }}
-                    />
-                  </div>
-                  <div className="flex gap-x-2 flex-wrap">
-                    <div className="bg-background px-2 rounded-md font-bold  w-fit">
-                      {media.OfficialRating}
+                        }/Images/Logo`}
+                        className="w-[200px] h-fit "
+                        alt={"Movie poster for " + media.Name}
+                        onError={() => {
+                          const element = document.getElementById(
+                            media.SeriesId ?? media.Id
+                          );
+                          element?.parentNode?.removeChild(element);
+                        }}
+                      />
                     </div>
-                    {media.hasSubtitles ? (
+                    <div className="flex gap-x-2 flex-wrap">
                       <div className="bg-background px-2 rounded-md font-bold  w-fit">
-                        Subtitles
+                        {media.OfficialRating}
                       </div>
-                    ) : null}
-                    <div className="bg-background px-2 rounded-md font-bold  w-fit">
-                      {new Date(media.PremiereDate).getFullYear()}
+                      {media.hasSubtitles ? (
+                        <div className="bg-background px-2 rounded-md font-bold  w-fit">
+                          Subtitles
+                        </div>
+                      ) : null}
+                      <div className="bg-background px-2 rounded-md font-bold  w-fit">
+                        {new Date(media.PremiereDate).getFullYear()}
+                      </div>
                     </div>
                   </div>
+                  <Link
+                    to={`/collection/${media.Type.toLowerCase()}/${
+                      media.SeriesId ?? media.Id
+                    }`}
+                    className="bg-primary hover:bg-blue-600 px-3 w-fit py-1 text-lg font-semibold rounded-md transition"
+                  >
+                    View
+                  </Link>
                 </div>
-                <Link
-                  to={`/collection/${media.Type.toLowerCase()}/${
+              </div>{" "}
+              <div
+                id={hashblur}
+                className="absolute z-20 top-0 left-0 bg-black"
+              >
+                <Blurhash hash={hashblur} width={600} height={377.5} />
+              </div>
+              <div>
+                <img
+                  width={600}
+                  className="h-fit "
+                  src={`${localStorage.getItem("address")}/Items/${
                     media.SeriesId ?? media.Id
-                  }`}
-                  className="bg-primary hover:bg-blue-600 px-3 w-fit py-1 text-lg font-semibold rounded-md transition"
-                >
-                  View
-                </Link>
+                  }/Images/Backdrop`}
+                  onLoad={() => {
+                    const element = document.getElementById(hashblur);
+                    element?.parentNode?.removeChild(element);
+                  }}
+                />
               </div>
             </div>
-            <img
-              width={600}
-              className="h-fit "
-              src={`${localStorage.getItem("address")}/Items/${
-                media.SeriesId ?? media.Id
-              }/Images/Backdrop`}
-            />
-          </div>
-        ))}
+          );
+        })}
       </Marquee>
     </div>
   );
