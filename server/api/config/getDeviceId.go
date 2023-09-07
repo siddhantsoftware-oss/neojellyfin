56b88f5755dad84cc6cc1fecfa63f5d70e780fd8
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-09-07 15:50:44 +0400

    Made "GetDeviceId" function save the new Id

--- server/api/config/getDeviceId.go
+++ server/api/config/getDeviceId.go
@@ -9,6 +9,7 @@ func GetDeviceId(c *gin.Context) {
 	deviceId, err := c.Cookie("deviceId")
 	if err != nil {
 		deviceId = uuid.NewString()
+		c.SetCookie("deviceId", deviceId, 100000000, "/", "localhost", false, true)
 	}
 	c.String(200, deviceId)
 }
