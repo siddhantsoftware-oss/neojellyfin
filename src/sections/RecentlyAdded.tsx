06acbea5b8a4370736b6c3cf2129d074bb4aa5c1
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-09-07 17:47:04 +0400

    Added details shown on hovering

--- src/sections/RecentlyAdded.tsx
+++ src/sections/RecentlyAdded.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { getAuth } from "../pages/root";
 import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 interface MediaType {
   Name: string;
@@ -10,6 +11,10 @@ interface MediaType {
   SortName: "string";
   SeriesId: string;
   SeriesName: string;
+  OfficialRating: string;
+  PremiereDate: Date;
+  hasSubtitles: boolean;
+  Type: string;
 }
 
 function RecentlyAdded() {
@@ -37,23 +42,46 @@ function RecentlyAdded() {
             className="shrink-0 group/cover relative transition overflow-clip mx-2 rounded-md"
           >
             <div className="absolute top-0 left-0 h-full w-full transition group-hover/cover:bg-black/40 group-hover/cover:backdrop-blur-sm ">
-              <div className="group-hover/cover:visible flex flex-col invisible transition p-5">
-                <div>
-                  <img
-                    id={media.SeriesId ?? media.Id}
-                    src={`${localStorage.getItem("address")}/Items/${
-                      media.SeriesId ?? media.Id
-                    }/Images/Logo`}
-                    className="w-[200px] h-fit "
-                    alt={"Movie poster for " + media.Name}
-                    onError={() => {
-                      const element = document.getElementById(
+              <div className="group-hover/cover:visible h-full flex flex-col justify-between invisible transition p-5">
+                <div className="flex flex-col gap-y-5">
+                  <div>
+                    <img
+                      id={media.SeriesId ?? media.Id}
+                      src={`${localStorage.getItem("address")}/Items/${
                         media.SeriesId ?? media.Id
-                      );
-                      element?.parentNode?.removeChild(element);
-                    }}
-                  />
+                      }/Images/Logo`}
+                      className="w-[200px] h-fit "
+                      alt={"Movie poster for " + media.Name}
+                      onError={() => {
+                        const element = document.getElementById(
+                          media.SeriesId ?? media.Id
+                        );
+                        element?.parentNode?.removeChild(element);
+                      }}
+                    />
+                  </div>
+                  <div className="flex gap-x-2 flex-wrap">
+                    <div className="bg-background px-2 rounded-md font-bold  w-fit">
+                      {media.OfficialRating}
+                    </div>
+                    {media.hasSubtitles ? (
+                      <div className="bg-background px-2 rounded-md font-bold  w-fit">
+                        Subtitles
+                      </div>
+                    ) : null}
+                    <div className="bg-background px-2 rounded-md font-bold  w-fit">
+                      {new Date(media.PremiereDate).getFullYear()}
+                    </div>
+                  </div>
                 </div>
+                <Link
+                  to={`/collection/${media.Type.toLowerCase()}/${
+                    media.SeriesId ?? media.Id
+                  }`}
+                  className="bg-primary hover:bg-blue-600 px-3 w-fit py-1 text-lg font-semibold rounded-md transition"
+                >
+                  View
+                </Link>
               </div>
             </div>
             <img
