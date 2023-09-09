63583fc93a5431e73d7a299e3592c0300b1ac34a
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-09-09 16:28:10 +0400

    Added flag on episodes to show if its finished

--- src/sections/media/AllEpisodes.tsx
+++ src/sections/media/AllEpisodes.tsx
@@ -24,6 +24,7 @@ function AllEpisodes({ media }: { media: Media }) {
             Id: string;
             UserData: {
               PlaybackPositionTicks: number;
+              Played: true;
             };
           }[]
       )
@@ -46,7 +47,7 @@ function AllEpisodes({ media }: { media: Media }) {
               episode.UserData.PlaybackPositionTicks
             }
             key={key}
-            className="flex hover:scale-105 transition group/cover flex-col  "
+            className="flex hover:scale-105 relative transition group/cover flex-col  "
           >
             <div className="relative">
               <div className="absolute invisible flex place-items-center justify-center rounded-md top-0 left-0 z-20 bg-black/60 group-hover/cover:visible  transition backdrop-blur-sm w-full h-full">
@@ -80,6 +81,22 @@ function AllEpisodes({ media }: { media: Media }) {
             <div className="text-sm font-semibold text-center">
               {episode.Name}
             </div>
+            {episode.UserData.Played ? (
+              <div className="absolute bg-black top-0 rounded-full right-0 p-1  ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6 text-green-500"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            ) : null}
           </Link>
         ))}
       </div>
